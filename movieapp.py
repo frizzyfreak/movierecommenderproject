d36c3e805cd455a01a6fd592ec7529b456524522
Ritik Singh <111752319+ritik-hue@users.noreply.github.com>
2024-11-07 12:18:24 +0530

    Update movieapp.py

--- movieapp.py
+++ movieapp.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as st
 import requests as re
 import pandas as pd
+from streamlit_lottie import st_lottie
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
